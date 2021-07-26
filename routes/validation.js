7146af7872ed657ba3616a4cc17a125fd264db50
dani198912 <bdaniel.mihalcea@gmail.com>
2021-07-26 09:36:58 +0300

    Fix header errors

--- routes/validation.js
+++ routes/validation.js
@@ -7,37 +7,44 @@ router.use(express.json());
 router.use(authentication);
 router.use(is_polygis_usable);
 
+//TODO Check double sent headers errors ... 2 times
+
 router
   .route("/")
   //	---------------- GET METHODS ---------------------
-
   .get((req, res) => {
     let is_ok = 1;
+    let body = req.body;
     //IF body in the request send an error
-    if (req.body == undefine) {
-      res.sendStatus(400);
+    if (body == undefined) {
+      res.status(400).send('Check the requests body.');
+    }else if (body.uuid == undefined) {
+      res.status(400).send('Check if body request contains UUID parameter.');
+    } else {
+      db.query(
+        `SELECT is_ok FROM stats WHERE (uuid = '${req.body.uuid}')`,
+        (err, results) => {
+          if (err) res.status(500).send(err.message);
+          is_ok = results[0]["is_ok"];
+          //IF users is_ok == 0 than deny access
+          if (is_ok == 0) {
+            res
+              .status(403)
+              .send(
+                "EROARE - Acces restricitionat, va rugam contactati admininstratorul."
+              );
+          }
+        }
+      );
+      let query_string = `SELECT id,version,mandatory_update,update_link FROM validation WHERE date = (SELECT MAX(date) from validation)`;
+      db.query(query_string, (err, results) => {
+        if (err) res.sendStatus(500);
+        console.log(results);
+        
+        res.sendStatus(200);
+      });
     }
 
-    db.query(
-      `SELECT is_ok FROM stats WHERE (uuid = '${req.body.uuid}')`,
-      (err, results) => {
-        console.log(results[0]["is_ok"]);
-        is_ok = results[0]["is_ok"];
-        if (is_ok == 0) {
-          res
-            .status(403)
-            .send(
-              "EROARE - Acces restricitionat, va rugam contactati admininstratorul."
-            );
-        }
-      }
-    );
-
-    let query_string = `SELECT id,version,mandatory_update,update_link FROM validation WHERE date = (SELECT MAX(date) from validation)`;
-    db.query(query_string, (err, results) => {
-      if (err) res.sendStatus(500);
-      res.status(200).send(results);
-    });
   });
 
 module.exports = router;
