a8a0ed74631fcae62a063fbd3f4bb534eb062f39
dani198912 <bdaniel.mihalcea@gmail.com>
2021-08-25 21:53:21 +0300

    Switched to maker-js because dxf writer is not working correctly

--- routes/dxf.js
+++ routes/dxf.js
@@ -1,33 +1,22 @@
 const express = require("express");
 const http = require("http");
-const Drawing = require("dxf-writer");
+const makerjs = require("makerjs");
+
 
 let router = express.Router();
 router.use(express.json());
 
 ConstructDXFFile = function (jsonData) {
-    let d = new Drawing();
-    d.setUnits("Decimeters");
-    d.drawText(10, 0, 10, 0, "polyGIS");
-    d.addLayer("l_green", Drawing.ACI.GREEN, "CONTINUOUS");
-    d.setActiveLayer("l_green");
-    d.drawText(20, -70, 10, 0, "go green!");
-
-    d.addLayer("polyLine", Drawing.ACI.BLUE, "CONTINUOUS");
-    d.setActiveLayer("polyLine");
-    d.drawPolyline(
-        [
-            [0, 0],
-            [10, 10],
-            [20, 10],
-            [30, 0],
-        ],
-        true,
-        1.5,
-        1.5
-    );
-
-    return d.toDxfString();
+    var line = { 
+        type: 'line', 
+        origin: [0, 0], 
+        end: [50, 50] 
+       };
+    var pathObj = {myLine: line};
+    var model = {paths:pathObj};
+    
+    var dxf = makerjs.exporter.toDXF(model)
+    return dxf
 };
 
 var requestJSON = "";
@@ -36,9 +25,10 @@ callback = function (response, res) {
     response.on("data", function (chunk) {
         requestJSON += chunk;
         requestJSON = JSON.parse(requestJSON);
-        dxf_file = ConstructDXFFile();
-        res.set("Content-disposition", `attachment; filename=dxf_ile.dxf`);
-        res.send(dxf_file);
+        var dxfFile = ConstructDXFFile();
+        
+        res.set("Content-disposition", `attachment; filename=dxf_file_maker.dxf`);
+        res.send(dxfFile);
     });
     response.on("end", function () {
         return;
