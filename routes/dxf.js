f0ba6fad6d0878128cea253fcb6775e469b083ab
Mihalcea Bogdan Daniel <dani_mihalcea@hotmail.com>
2021-09-09 17:53:25 +0300

    Cors enabled

--- routes/dxf.js
+++ routes/dxf.js
@@ -1,14 +1,21 @@
 const express = require("express");
-const http = require("http");
 const makerjs = require("makerjs");
+const cors = require("cors");
 const db = require("../sqlconnection.js");
 let router = express.Router();
+var corsOptions = {
+    origin: "https://geoportal.ancpi.ro",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+router.use(cors(corsOptions));
 router.use(express.json());
 
-ConstructDXFFile = function (jsonData) {
+ConstructDXFFile = function (pointsArray) {
     try {
-        points = jsonData["features"][0]["geometry"]["rings"][0];
-        var cadastral_points = new makerjs.models.ConnectTheDots(true, points);
+        var cadastral_points = new makerjs.models.ConnectTheDots(
+            true,
+            pointsArray
+        );
         var model = {
             models: { cadastru: cadastral_points },
         };
@@ -17,82 +24,42 @@ ConstructDXFFile = function (jsonData) {
 
         return dxf;
     } catch (error) {
-        return error;
+        return new Error(error.message);
     }
 };
 
-var requestJSON = "";
-callback = function (
-    response,
-    req,
-    res,
-    denumire_judet,
-    denumire_localitate,
-    numar_cadastral
-) {
-    requestJSON = "";
-    response.on("data", function (chunk) {
-        requestJSON += chunk;
-        requestJSON = JSON.parse(requestJSON);
-        try {
-            points = requestJSON["features"][0]["geometry"]["rings"][0];
-            var cadastral_points = new makerjs.models.ConnectTheDots(
-                true,
-                points
-            );
-            var model = {
-                models: { cadastru: cadastral_points },
-            };
-            var dxf = makerjs.exporter.toDXF(model);
-            res.set(
-                "Content-disposition",
-                `attachment; filename=${denumire_judet}-${denumire_localitate}-${numar_cadastral}.dxf`
-            );
-
-            const insertHistoryQuery = `INSERT INTO search_history (judet, localitate, numar_cadastral, uuid) 
+router.route("/").post((req, res) => {
+    try {
+        if (!req.body.points) {
+            throw new Error("NO POINTS");
+        }
+        if (!req.body.numar_cadastral) {
+            throw new Error("NO CADASTER FOUND");
+        }
+        const dxf = ConstructDXFFile(req.body.points);
+        const insertHistoryQuery = `INSERT INTO search_history (judet, localitate, numar_cadastral, uuid) 
   VALUES (
       '${req.body.judet_id}', 
       '${req.body.localitate_uat}', 
       '${req.body.numar_cadastral}',
-      'api.polygis.xyz-not_required_uuid')`;
-            db.query(insertHistoryQuery, (err, results) => {
-                if (err) {
-                    console.log(err);
-                } else {
-                    console.log("Inserted scuccessfully!");
-                }
-            });
-
-            res.status(200).send(dxf);
-        } catch (error) {
-            res.status(500).send(error.message);
-        }
-    });
-    response.on("end", function () {
-        return;
-    });
-};
-
-router.route("/").post((req, res) => {
-    var options = {
-        host: "geoportal.ancpi.ro",
-        path: `/maps/rest/services/eterra3_publish/MapServer/1/query?f=json&where=INSPIRE_ID='RO.${req.body.judet_id}.${req.body.localitate_uat}.${req.body.numar_cadastral}'&returnGeometry=true&spatialRel=esriSpatialRelIntersects&outFields=NATIONAL_CADASTRAL_REFERENCE`,
-        headers: {
-            Origin: "http://geoportal.ancpi.ro",
-            Referer: "https://geoportal.ancpi.ro/geoportal/imobile/Harta.html",
-        },
-    };
-    var httpReq = http.request(options, (response) => {
-        callback(
-            response,
-            req,
-            res,
-            req.body.denumire_judet,
-            req.body.denumire_localitate,
-            req.body.numar_cadastral
+      '${req.body.uuid}')`;
+        db.query(insertHistoryQuery, (err) => {
+            if (err) {
+                res.status(500).send(
+                    "MySQL Error, please contact administrator."
+                );
+                return;
+            }
+            console.log("Inserted entry");
+        });
+        res.setHeader(
+            "Content-disposition",
+            "attachment; filename=" + `${req.body.numar_cadastral}.dxf`
         );
-    });
-    httpReq.end();
+        res.status(200).send(dxf);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
 });
 
 module.exports = router;
