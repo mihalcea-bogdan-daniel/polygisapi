7e48fdda72a6ee1130f10291cf25027a4b9f9f91
Mihalcea Bogdan Daniel <dani_mihalcea@hotmail.com>
2021-09-16 18:10:05 +0300

    Added polygis module, repaired some bugs

--- routes/GISTransformations.js
+++ routes/GISTransformations.js
@@ -1,21 +1,7 @@
 const express = require("express");
-const {
-    StereoToETRS89,
-    ETRS89ToStereo,
-} = require("../modules/Stereo70ToETRS89.js");
+
 let router = express.Router();
 router.route("/stereo_to_etrs89").get((req, res) => {
-    let tr = new StereoToETRS89("name");
-    // let interp = new Interpolation2D(
-    //     325544,
-    //     522355,
-    //     "ETRS89_KRASOVSCHI42_2DJ.GRD"
-    // );
-    tr.DoTransformation(585641, 345647, 70);
-    let tre = new ETRS89ToStereo("name");
-    tre.DoTransformation(46, 25, 70);
-    res.status(200).send(
-        `Stereo 70  -> Etrs89 \nN: ${tr.GetPhi}\nN: ${tre.GetLa}`
-    );
+    res.status(200).send(`Stereo 70  -> Etrs89 :`);
 });
 module.exports = router;
