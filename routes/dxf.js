a26a254cfbab8cba7d9e3a664c21d48ee8599045
dani198912 <bdaniel.mihalcea@gmail.com>
2021-09-16 20:31:14 +0300

    Added polygon registry

--- routes/dxf.js
+++ routes/dxf.js
@@ -2,6 +2,7 @@ const express = require("express");
 const makerjs = require("makerjs");
 const cors = require("cors");
 const db = require("../sqlconnection.js");
+const polygis = require("polygis");
 let router = express.Router();
 var corsOptions = {
     origin: "https://geoportal.ancpi.ro",
@@ -9,6 +10,20 @@ var corsOptions = {
 };
 router.use(cors(corsOptions));
 router.use(express.json());
+
+ConstructGISData = function (pointsArray){
+    let stringOfSQLPolygon='POLYGON((';
+    pointsArray.forEach((p, i)=>{
+        let pointETRS89 = polygis.ConvertStereoToETRS89(p[0],p[1]);
+        let pointString = `${pointETRS89.phi} ${pointETRS89.la}`
+        if(i < pointsArray.length && i>0){
+            stringOfSQLPolygon += ','
+        }
+        stringOfSQLPolygon += pointString;
+    });
+    stringOfSQLPolygon += "))";
+    return stringOfSQLPolygon;
+}
 
 ConstructDXFFile = function (pointsArray) {
     try {
@@ -37,18 +52,21 @@ router.route("/").post((req, res) => {
             throw new Error("NO CADASTER FOUND");
         }
         const dxf = ConstructDXFFile(req.body.points);
-        const insertHistoryQuery = `INSERT INTO search_history (judet, localitate, numar_cadastral, uuid) 
+        const SQLPolygonString = ConstructGISData(req.body.points);
+        console.log(SQLPolygonString);
+        const insertHistoryQuery = `INSERT INTO search_history (judet, localitate, numar_cadastral, polygon_numar_cadastral, uuid) 
   VALUES (
       '${req.body.judet_id}', 
       '${req.body.localitate_uat}', 
       '${req.body.numar_cadastral}',
+      ST_PolygonFromText('${SQLPolygonString}'),
       '${req.body.uuid}')`;
         db.query(insertHistoryQuery, (err) => {
             if (err) {
                 res.status(500).send(
                     "MySQL Error, please contact administrator."
                 );
-                return;
+                throw new Error(err.message);
             }
             console.log("Inserted entry");
         });
