0a776e81fa0108b7e6929c3223bab07fc0622174
Mihalcea Bogdan Daniel <dani_mihalcea@hotmail.com>
2021-09-20 10:26:35 +0300

    Reorganize transforms

--- views/pug/js/SubmitRequestRequest.js
+++ views/pug/js/SubmitRequestRequest.js
@@ -1,33 +1,33 @@
 let submitForm = document.querySelector(".submit-form");
 let submitButton = document.querySelector(".submit-button");
 
-
-
 submitForm.onsubmit = function (e) {
     e.preventDefault();
     let coordinatesElement = document.querySelector("#coordinates-textarea");
     let coordString = coordinatesElement.value;
-    let typeSelection = document.querySelector(".type-select")
-    // let request_url = `https://api.polygis.xyz/transforms/stereo_to_etrs89/`;
-    let request_url = `http://localhost:5000/transforms/stereo_to_etrs89/`;
+    let typeSelection = document.querySelector(".type-select");
+    let request_url = window.location.href;
+    //let request_url = `http://localhost:5000/transforms/stereo_to_etrs89/`;
+    console.log(window.location.href);
     req_body = {
-        "coordinates": coordString,
-        "type": typeSelection.value
-    }
-    fetch(request_url,
-        {
-            method: "POST",
-            mode: "cors",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(req_body),
-
-        }).then((resp) => {
-            console.log(resp.status);
+        coordinates: coordString,
+        type: typeSelection.value,
+    };
+    fetch(request_url, {
+        method: "POST",
+        mode: "cors",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(req_body),
+    })
+        .then((resp) => {
             return resp.json();
-        }).then((json) => {
-            console.log(json)
+        })
+        .then((json) => {
+            let responseElement = document.querySelector(".response");
+            responseElement.textContent = JSON.stringify(json);
+            console.log(responseElement);
+            hljs.highlightAll();
         });
-
-}
+};
